11cf9c8f1b91dfe2dbeb2526b0915d8aff167111
AnmolThh <at2558392@gmail.com>
2025-06-11 12:58:43 +0530

    second commit

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -21,7 +21,7 @@ const SignIn = () => {
   return (
     <>
       {/* CONTAINER */}
-      <div className="w-[130vh] relative bg-container flex rounded-md overflow-hidden shadow-xl">
+      <div className="w-[95%]  md:w-[130vh] relative bg-container flex rounded-md overflow-hidden shadow-xl">
         {/* lEFT-CONTAINER */}
         <div className="h-inherit flex-1 flex flex-col items-center justify-center">
           {/* THEME TOGGLE */}
@@ -90,7 +90,7 @@ const SignIn = () => {
                         type="text"
                         key={index}
                         placeholder={item.title}
-                        className="border-2 border-background h-13 text-sm font-medium text-primary opacity-70 w-[60%] pl-25 rounded-xl placeholder:text-xs placeholder:text-ternary shadow-md"
+                        className="border-2 border-secondary h-13 text-sm font-medium text-primary opacity-70 w-[65%] px-10 rounded-xl placeholder:text-xs placeholder:text-ternary shadow-md"
                       />
                     );
                   })
@@ -99,38 +99,38 @@ const SignIn = () => {
                       type={item.icon === "KeyRound" ? "password" : "text"}
                       key={index}
                       placeholder={item.title}
-                      className="border-2 border-background h-13 text-sm font-medium text-primary opacity-70 w-[60%] pl-25 rounded-xl placeholder:text-xs placeholder:text-ternary shadow-md"
+                      className="border-2 border-secondary h-13 text-sm font-medium text-primary opacity-70 w-[65%] px-10 rounded-xl placeholder:text-xs placeholder:text-ternary shadow-md"
                     />
                   ))}
-              <button className="bg-btn h-13 w-[60%] rounded-xl text-container text-sm shadow-3xl hover:shadow-lg hover:scale-95">
+              <button className="bg-btn h-13 w-[65%] rounded-xl text-container text-sm shadow-3xl hover:shadow-lg hover:scale-95">
                 Continue
               </button>
             </form>
             {/* LINKS */}
-            <div className="w-full flex flex-col items-center">
+            <div className="w-full flex flex-col items-center ">
               {/* OTHERS SECTION */}
-              <div className="w-[60%] py-5 relative ">
-                <p className="absolute text-ternary text-xs font-middle text-center bg-container left-[35%] top-3 px-1">
+              <div className="w-full py-5 relative flex justify-center">
+                <p className="absolute text-ternary text-xs font-middle text-center bg-container top-3 px-1">
                   Or Continue With
                 </p>
-                <hr className="border-background " />
+                <hr className="border-background w-[60%] " />
               </div>
 
               {/* LINKS */}
               <div className="flex gap-5">
                 <img
                   src={Google}
-                  alt=""
+                  alt="google_img"
                   className="size-10 border-1 border-secondary rounded-full duration-75 hover:translate-y-[-5px]"
                 />
                 <img
                   src={Apple}
-                  alt=""
+                  alt="Apple_img"
                   className="size-10 px-2.5 py-2 rounded-full bg-black duration-75 hover:translate-y-[-5px]"
                 />
                 <img
                   src={Facebook}
-                  alt=""
+                  alt="Facebook_img"
                   className="size-10 hover:translate-y-[-5px] "
                 />
               </div>
@@ -139,7 +139,7 @@ const SignIn = () => {
 
           {/* FOOTER */}
           <div className="">
-            <p className="text-secondary text-xs text-center px-20 py-8">
+            <p className="text-secondary text-[0.6rem] shadow-sky-500 text-center px-19 py-8">
               Join the millions of smart investors who trust us to manage their
               finances. Log in to access your personalized dashboard, track your
               portfolio performance, and make informed investment decisions
@@ -148,7 +148,7 @@ const SignIn = () => {
         </div>
 
         {/* RIGHT CONTAINER */}
-        <div className="bg-blue-300 h-inherit flex-1 bg-linear-to-br from-blue-500 to-blue-300"></div>
+        <div className=" bg-blue-300 h-inherit md:flex-1 bg-linear-to-bl from-blue-700 to-blue-400"></div>
       </div>
     </>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: [
+      "0776-2401-4900-882b-163-60d0-6bae-2ac9-a7e4.ngrok-free.app",
+    ],
+  },
 });
